98480828883722426d8a1d2db4aba172e8d46091
Isui <isui@localhost.localdomain>
2016-11-10 12:28:53 -0600

    imagemin of gruntfile

--- SPA/Gruntfile.js
+++ SPA/Gruntfile.js
@@ -301,7 +301,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif,svg}',
+          src: ['{,*/}*.{png,jpg,jpeg,gif,svg}','images/**/*.{png,jpg,jpeg,gif,svg}'],
           dest: '<%= yeoman.dist %>/images'
         }]
       }
